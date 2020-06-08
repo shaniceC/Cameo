cccc7d70e65b9a7e029d7e236c486f653624b6dc
Shanice <shaniceclk@gmail.com>
2020-06-08 14:56:53 -0400

    Add functions to make applying functions to images more efficient

--- utils.py
+++ utils.py
@@ -21,3 +21,61 @@ def createCurveFunc(points):
         kind = 'cubic'
 
     return scipy.interpolate(xs, ys, kind, bounds_error=False)
+
+
+def createLookupArray(func, length=256):
+    """ Return a lookup for whole-number inputs to a function.
+        The lookup values are clamped to [0, length - 1].
+    """
+
+    if func is None:
+        return None
+
+    lookupArry = np.empty(length)
+    i = 0
+    while i < length:
+        func_i = func(i)
+        lookupArry[i] = min(max(0, func_i), length - 1)
+        i += 1
+
+    return lookupArry
+
+
+def applyLookupArray(lookupArray, src, dst):
+    """ Map a source to destination using a lookup. """
+    if lookupArray is None:
+        return
+
+    dst[:] = lookupArray[src]
+
+
+def createCompositeFunc(func0, func1):
+    """ Return a composite of two functions. """
+    if func0 is None:
+        return func1
+    if func1 is None:
+        return func0
+
+    return lambda x: func0(func1(x))
+
+
+def createFlatView(array):
+    """ Return a 1D view of an array of any dimensionality. """
+    flatView = array.view()
+    flatView.shape = array.size
+    return flatView
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
